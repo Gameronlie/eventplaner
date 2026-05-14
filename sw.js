0895809a21e9ba5e4876236b581a11ff9356737b
Gameronlie <132715167+Gameronlie@users.noreply.github.com>
2026-05-14 02:03:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('event-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/Gemini_Generated_Image_bu298gbu298gbu29.png'
-    ]))
-  );
+const cacheName = 'portal-v1';
+const assets = ['./', './index.html', './manifest.json', './Gemini_Generated_Image_bu298gbu298gbu29.png'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
