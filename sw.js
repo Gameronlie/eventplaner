be617124e068dd09df348a04b7e4ff01a6b0aa06
Gameronlie <132715167+Gameronlie@users.noreply.github.com>
2026-05-14 02:19:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const cacheName = 'portal-v1';
-const assets = ['./', './index.html', './manifest.json', './Gemini_Generated_Image_bu298gbu298gbu29.png'];
+const CACHE_NAME = 'portal-v1';
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Zwingt die App, den neuen Code sofort zu aktivieren
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((names) => {
+            return Promise.all(names.map(name => caches.delete(name)));
+        })
+    );
+});
+
+// Netzwerk-Zuerst-Strategie: Holt immer den neuesten Code vom Server
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
